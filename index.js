87e27f151475524f8d748f5ce66df41436b5a0ef
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2023-10-28 16:44:43 +0300

    auto-width texarea

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-  let dialog = document.querySelector('dialog')
+document.querySelectorAll('textarea').forEach(el => {
+    el.style.height = el.setAttribute('style', 'height: ' + el.scrollHeight + 'px');
+    el.classList.add('auto');
+    el.addEventListener('input', e => {
+        el.style.height = 'auto';
+        el.style.height = (el.scrollHeight) + 'px';
+        console.log(el.scrollHeight)
+    });
+});
+
+let dialog = document.querySelector('dialog')
   // выводим диалоговое окно
   document.querySelector('#open').onclick = function () {
     dialog.showModal()
@@ -7,3 +17,4 @@
   document.querySelector('#close').onclick = function () {
     dialog.close() 
   }
+
