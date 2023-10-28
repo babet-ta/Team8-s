5adb5754531b307b413820329de50c4507c6e461
Gulnaz <gulnaz.baembitova@mail.ru>
2023-10-28 15:25:17 +0300

    carusel

---
+++ index.js
@@ -0,0 +1,21 @@
+let quoteContainer = document.getElementById('quoteContainer');
+let quotes = quoteContainer.getElementsByClassName('quote');
+let quoteIndex = 0;
+const interval = 3000; // Задержка в 3 секунды
+
+function rotateQuotes() {
+  // Скрываем текущую цитату
+  quotes[quoteIndex].style.display = 'none';
+  
+  // Увеличиваем индекс цитаты или сбрасываем на 0, если достигнут конец
+  quoteIndex = (quoteIndex + 1) % quotes.length;
+  
+  // Показываем следующую цитату
+  quotes[quoteIndex].style.display = 'block';
+  
+  // Устанавливаем задержку перед следующим пролистыванием
+  setTimeout(rotateQuotes, interval);
+}
+
+// Запускаем пролистывание цитат
+rotateQuotes();
